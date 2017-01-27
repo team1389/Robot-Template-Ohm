38af556283b432639894b0b67f58ed5d44d0684b
Ari Mindell <amindell11@gmail.com>
2017-01-27 09:26:21 -0500

    fixed issues with latest ohm integration

--- Robot-Template-Ohm/src/org/usfirst/frc/team1389/watchers/DebugDash.java
+++ Robot-Template-Ohm/src/org/usfirst/frc/team1389/watchers/DebugDash.java
@@ -23,10 +23,4 @@ public class DebugDash extends Watcher {
 		publish(Watcher.DASHBOARD);
 	}
 
-	@Override
-	public void clearWatchers() {
-		// TODO doesnt do anything
-		super.clearWatchers();
-	}
-
 }
