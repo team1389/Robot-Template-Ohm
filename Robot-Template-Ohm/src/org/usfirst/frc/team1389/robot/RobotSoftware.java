65025d4e04497041e9d66622b07182967e537645
RaphaelMetz <raphaeljmetz@gmail.com>
2017-11-15 15:06:10 -0500

    Removed threading

--- Robot-Template-Ohm/src/org/usfirst/frc/team1389/robot/RobotSoftware.java
+++ Robot-Template-Ohm/src/org/usfirst/frc/team1389/robot/RobotSoftware.java
@@ -1,17 +1,16 @@
 package org.usfirst.frc.team1389.robot;
 
-import com.team1389.concurrent.OhmThreadService;
-
-public class RobotSoftware extends RobotHardware {
+public class RobotSoftware extends RobotHardware
+{
 	private static RobotSoftware INSTANCE = new RobotSoftware();
 
-	public static RobotSoftware getInstance() {
+	public static RobotSoftware getInstance()
+	{
 		return INSTANCE;
 	}
 
-	public RobotSoftware() {
-		threadService = new OhmThreadService(RobotConstants.MaxConcurrentThreads);
+	public RobotSoftware()
+	{
 	}
 
-	public OhmThreadService threadService;
 }
